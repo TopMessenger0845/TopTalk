a6eb52ddd096fbdceb4e6e2854f8646280a2ffdc
Artifait <am3044288@gmail.com>
2025-01-22 12:48:43 +0700

    Save

--- Core/Models/StatusDataModel.cs
+++ Core/Models/StatusDataModel.cs
@@ -4,9 +4,9 @@ namespace TopTalk.Core.Models
     public class StatusDataModel
     {
         public string ContactName { get; set; }
-        public Uri ContactPhoto { get; set; }
-        public string StatusMessage { get; set; }
-        public Uri StatusImage { get; set; }
-        public bool IsMeAddStatus { get; set; }
+        public Uri ContactPhoto { get; set; } = new("/Assets/2.jpg", UriKind.RelativeOrAbsolute);
+        // public string StatusMessage { get; set; }
+        public Uri StatusImage { get; set; } = new("/Assets/1.jpg", UriKind.RelativeOrAbsolute);
+        public bool IsMeAddStatus { get; set; } = false;
     }
 }
--- Core/ViewModels/ViewModel.cs
+++ Core/ViewModels/ViewModel.cs
@@ -1,10 +1,20 @@
 ﻿
 using System.Collections.ObjectModel;
+using TopTalk.Core.Models;
 
 namespace TopTalk.Core.ViewModels
 {
     public class ViewModel
     {
-        //ObservableCollection<StatusTh>
+        public ObservableCollection<StatusDataModel> StatusThumbsCollection { get; set; }
+        public ViewModel() 
+        {
+            StatusThumbsCollection =
+            [
+                new() {ContactName="Art"},
+                new() {ContactName="TopTalk"},
+                new() {ContactName="Arsyha"},
+            ];
+        }
     }
 }
